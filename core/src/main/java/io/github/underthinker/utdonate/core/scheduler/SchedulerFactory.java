a4f49fdf2dc3bba2cfbcef575685df8a5acde4f9
HSGamer <huynhqtienvtag@gmail.com>
2022-09-25 23:37:41 +0700

    default on runTask

--- core/src/main/java/io/github/underthinker/utdonate/core/scheduler/SchedulerFactory.java
+++ core/src/main/java/io/github/underthinker/utdonate/core/scheduler/SchedulerFactory.java
@@ -1,15 +1,19 @@
 package io.github.underthinker.utdonate.core.scheduler;
 
 public interface SchedulerFactory {
-    SchedulerTask runTask(Runnable runnable);
-
     SchedulerTask runTaskTimer(Runnable runnable, long delay, long period);
 
     SchedulerTask runTaskLater(Runnable runnable, long delay);
 
-    SchedulerTask runTaskAsynchronously(Runnable runnable);
+    default SchedulerTask runTask(Runnable runnable) {
+        return runTaskLater(runnable, 0);
+    }
 
     SchedulerTask runTaskTimerAsynchronously(Runnable runnable, long delay, long period);
 
     SchedulerTask runTaskLaterAsynchronously(Runnable runnable, long delay);
+
+    default SchedulerTask runTaskAsynchronously(Runnable runnable) {
+        return runTaskLaterAsynchronously(runnable, 0);
+    }
 }
