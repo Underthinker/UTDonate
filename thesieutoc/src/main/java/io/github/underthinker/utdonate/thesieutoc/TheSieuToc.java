644193737627b717cf123ba6eec48b09e13dfba3
HSGamer <huynhqtienvtag@gmail.com>
2022-10-02 23:50:44 +0700

    begin TheSieuToc

---
+++ thesieutoc/src/main/java/io/github/underthinker/utdonate/thesieutoc/TheSieuToc.java
@@ -0,0 +1,15 @@
+package io.github.underthinker.utdonate.thesieutoc;
+
+import io.github.underthinker.utdonate.core.entity.addon.DonateAddon;
+
+public class TheSieuToc extends DonateAddon {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
