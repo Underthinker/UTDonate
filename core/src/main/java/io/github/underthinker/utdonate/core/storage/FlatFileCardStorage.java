b0931d2a5e547f32b80329fc8dceffdbd3e6dbbd
HSGamer <huynhqtienvtag@gmail.com>
2022-10-04 01:00:40 +0700

    split to MapBasedCardStorage

--- core/src/main/java/io/github/underthinker/utdonate/core/storage/FlatFileCardStorage.java
+++ core/src/main/java/io/github/underthinker/utdonate/core/storage/FlatFileCardStorage.java
@@ -3,8 +3,6 @@ package io.github.underthinker.utdonate.core.storage;
 import io.github.underthinker.utdonate.core.UTDonateCore;
 import io.github.underthinker.utdonate.core.entity.card.Card;
 import io.github.underthinker.utdonate.core.entity.card.CardData;
-import io.github.underthinker.utdonate.core.entity.storage.CardStorage;
-import io.github.underthinker.utdonate.core.scheduler.SchedulerTask;
 import me.hsgamer.hscore.common.Pair;
 
 import java.io.File;
@@ -12,23 +10,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
 
-public class FlatFileCardStorage implements CardStorage {
-    private final UTDonateCore core;
-    private final File file;
-    private final Map<Long, CardData> cards = new ConcurrentHashMap<>();
-    private final AtomicBoolean isSaving = new AtomicBoolean(false);
-    private final AtomicReference<SchedulerTask> currentTask = new AtomicReference<>();
-
-    public FlatFileCardStorage(UTDonateCore core, CardStorage.Input input) {
-        this.core = core;
-        this.file = new File(core.getCardStorageManager().getBaseFolder(), input.getName() + ".cards");
+public class FlatFileCardStorage extends MapBasedCardStorage {
+    public FlatFileCardStorage(UTDonateCore core, Input input) {
+        super(core, input);
     }
 
     private static String serializeCardData(Pair<Long, CardData> cardDataPair) {
@@ -66,90 +52,33 @@ public class FlatFileCardStorage implements CardStorage {
     }
 
     @Override
-    public boolean setup() {
-        if (!file.exists()) {
-            try {
-                Files.createFile(file.toPath());
-            } catch (IOException e) {
-                core.getLogger().log(Level.SEVERE, e, () -> "Failed to create file " + file.getName());
-                return false;
-            }
-        }
+    protected String getFileExtension() {
+        return "txt";
+    }
+
+    @Override
+    protected Map<Long, CardData> load(File file) {
+        Map<Long, CardData> map = new HashMap<>();
         try {
             List<String> lines = Files.readAllLines(file.toPath());
             for (String line : lines) {
                 Pair<Long, CardData> cardDataPair = deserializeCardData(line);
-                cards.put(cardDataPair.getKey(), cardDataPair.getValue());
+                map.put(cardDataPair.getKey(), cardDataPair.getValue());
             }
         } catch (IOException e) {
             core.getLogger().log(Level.WARNING, e, () -> "Failed to read file " + file.getName());
         }
-        return true;
+        return map;
     }
 
     @Override
-    public void stop() {
-        Optional.ofNullable(currentTask.get()).ifPresent(SchedulerTask::cancel);
-        if (!isSaving.get()) {
-            save();
-        }
-    }
-
-    private void save() {
+    protected void save(File file, Map<Long, CardData> map) {
         try {
             List<String> list = new ArrayList<>();
-            cards.forEach((id, cardData) -> list.add(serializeCardData(Pair.of(id, cardData))));
+            map.forEach((id, cardData) -> list.add(serializeCardData(Pair.of(id, cardData))));
             Files.write(file.toPath(), list, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
         } catch (IOException e) {
             core.getLogger().log(Level.SEVERE, e, () -> "Failed to save file " + file.getName());
         }
-    }
-
-    private void scheduleSave() {
-        if (isSaving.get()) return;
-        isSaving.set(true);
-        currentTask.set(core.getSchedulerFactory().runTaskLater(() -> {
-            save();
-            isSaving.set(false);
-        }, 40));
-    }
-
-    @Override
-    public CompletableFuture<Map<Long, CardData>> getAll() {
-        return CompletableFuture.completedFuture(Collections.unmodifiableMap(cards));
-    }
-
-    @Override
-    public CompletableFuture<Map<Long, CardData>> getByOwner(UUID ownerId) {
-        return CompletableFuture.supplyAsync(() ->
-                cards.entrySet().stream()
-                        .filter(entry -> Objects.equals(entry.getValue().getCard().getOwnerId(), ownerId))
-                        .collect(HashMap::new, (map, entry) -> map.put(entry.getKey(), entry.getValue()), HashMap::putAll)
-        );
-    }
-
-    @Override
-    public CompletableFuture<CardData> get(long id) {
-        return CompletableFuture.completedFuture(cards.get(id));
-    }
-
-    @Override
-    public CompletableFuture<Void> save(CardData card) {
-        return CompletableFuture.runAsync(() -> {
-            long nextId;
-            do {
-                nextId = ThreadLocalRandom.current().nextLong();
-            } while (cards.containsKey(nextId));
-            cards.put(nextId, card);
-            scheduleSave();
-        });
-    }
-
-    @Override
-    public CompletableFuture<Void> remove(long id) {
-        return CompletableFuture.runAsync(() -> {
-            cards.remove(id);
-            scheduleSave();
-        });
     }
 }
---
+++ core/src/main/java/io/github/underthinker/utdonate/core/storage/MapBasedCardStorage.java
@@ -0,0 +1,111 @@
+package io.github.underthinker.utdonate.core.storage;
+
+import io.github.underthinker.utdonate.core.UTDonateCore;
+import io.github.underthinker.utdonate.core.entity.card.CardData;
+import io.github.underthinker.utdonate.core.entity.storage.CardStorage;
+import io.github.underthinker.utdonate.core.scheduler.SchedulerTask;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.Level;
+
+public abstract class MapBasedCardStorage implements CardStorage {
+    protected final UTDonateCore core;
+    private final File file;
+    private final Map<Long, CardData> cards = new ConcurrentHashMap<>();
+    private final AtomicBoolean isSaving = new AtomicBoolean(false);
+    private final AtomicReference<SchedulerTask> currentTask = new AtomicReference<>();
+
+    protected MapBasedCardStorage(UTDonateCore core, CardStorage.Input input) {
+        this.core = core;
+        this.file = new File(core.getCardStorageManager().getBaseFolder(), input.getName() + "." + getFileExtension());
+    }
+
+    protected abstract String getFileExtension();
+
+    protected abstract Map<Long, CardData> load(File file);
+
+    protected abstract void save(File file, Map<Long, CardData> map);
+
+    @Override
+    public boolean setup() {
+        if (!file.exists()) {
+            try {
+                Files.createFile(file.toPath());
+            } catch (IOException e) {
+                core.getLogger().log(Level.SEVERE, e, () -> "Failed to create file " + file.getName());
+                return false;
+            }
+        }
+        cards.putAll(load(file));
+        return true;
+    }
+
+    @Override
+    public void stop() {
+        Optional.ofNullable(currentTask.get()).ifPresent(SchedulerTask::cancel);
+        if (!isSaving.get()) {
+            save();
+        }
+    }
+
+    private void save() {
+        Map<Long, CardData> map = new HashMap<>(cards);
+        save(file, map);
+    }
+
+    private void scheduleSave() {
+        if (isSaving.get()) return;
+        isSaving.set(true);
+        currentTask.set(core.getSchedulerFactory().runTaskLater(() -> {
+            save();
+            isSaving.set(false);
+        }, 40));
+    }
+
+    @Override
+    public CompletableFuture<Map<Long, CardData>> getAll() {
+        return CompletableFuture.completedFuture(Collections.unmodifiableMap(cards));
+    }
+
+    @Override
+    public CompletableFuture<Map<Long, CardData>> getByOwner(UUID ownerId) {
+        return CompletableFuture.supplyAsync(() ->
+                cards.entrySet().stream()
+                        .filter(entry -> Objects.equals(entry.getValue().getCard().getOwnerId(), ownerId))
+                        .collect(HashMap::new, (map, entry) -> map.put(entry.getKey(), entry.getValue()), HashMap::putAll)
+        );
+    }
+
+    @Override
+    public CompletableFuture<CardData> get(long id) {
+        return CompletableFuture.completedFuture(cards.get(id));
+    }
+
+    @Override
+    public CompletableFuture<Void> save(CardData card) {
+        return CompletableFuture.runAsync(() -> {
+            long nextId;
+            do {
+                nextId = ThreadLocalRandom.current().nextLong();
+            } while (cards.containsKey(nextId));
+            cards.put(nextId, card);
+            scheduleSave();
+        });
+    }
+
+    @Override
+    public CompletableFuture<Void> remove(long id) {
+        return CompletableFuture.runAsync(() -> {
+            cards.remove(id);
+            scheduleSave();
+        });
+    }
+}
