bda50628f71c581f5d02b29dbe13f162d2be99a1
HSGamer <huynhqtienvtag@gmail.com>
2022-10-08 22:25:07 +0700

    simpler Log4j2Handler

--- impl-sponge/src/main/java/io/github/underthinker/utdonate/sponge/Log4j2Handler.java
+++ impl-sponge/src/main/java/io/github/underthinker/utdonate/sponge/Log4j2Handler.java
@@ -2,59 +2,39 @@ package io.github.underthinker.utdonate.sponge;
 
 import org.apache.logging.log4j.Logger;
 
-import java.util.logging.Formatter;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
 
 public class Log4j2Handler extends Handler {
     private final Logger logger;
 
     public Log4j2Handler(Logger logger) {
         this.logger = logger;
-        setFormatter(new Formatter() {
-            @Override
-            public String format(LogRecord record) {
-                StringBuilder builder = new StringBuilder();
-                builder.append(formatMessage(record));
-                Throwable throwable = record.getThrown();
-                if (throwable != null) {
-                    builder.append("\n");
-                    builder.append(throwable.getClass().getName()).append(": ").append(throwable.getMessage()).append("\n");
-                    for (StackTraceElement element : throwable.getStackTrace()) {
-                        builder.append("  at ")
-                                .append(element.getClassName())
-                                .append(" ")
-                                .append(element.getFileName())
-                                .append(":")
-                                .append(element.getLineNumber())
-                                .append("\n");
-                    }
-                }
-                return builder.toString();
-            }
-        });
+        setFormatter(new SimpleFormatter());
     }
 
     @Override
     public void publish(LogRecord record) {
-        String message = getFormatter().format(record);
+        String message = getFormatter().formatMessage(record);
+        Throwable throwable = record.getThrown();
         switch (record.getLevel().getName()) {
             case "SEVERE":
-                logger.error(message);
+                logger.error(message, throwable);
                 break;
             case "WARNING":
-                logger.warn(message);
+                logger.warn(message, throwable);
                 break;
             case "CONFIG":
             case "FINE":
-                logger.debug(message);
+                logger.debug(message, throwable);
                 break;
             case "FINER":
             case "FINEST":
-                logger.trace(message);
+                logger.trace(message, throwable);
                 break;
             default:
-                logger.info(message);
+                logger.info(message, throwable);
                 break;
         }
     }
