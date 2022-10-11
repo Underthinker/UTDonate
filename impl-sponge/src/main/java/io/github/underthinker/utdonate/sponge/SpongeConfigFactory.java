aba03433ffe0a84c0cff185fac5cedb2f3827c73
HSGamer <huynhqtienvtag@gmail.com>
2022-10-12 00:06:49 +0700

    correct file extension for HOCON

--- impl-sponge/src/main/java/io/github/underthinker/utdonate/sponge/SpongeConfigFactory.java
+++ impl-sponge/src/main/java/io/github/underthinker/utdonate/sponge/SpongeConfigFactory.java
@@ -16,6 +16,6 @@ public class SpongeConfigFactory implements ConfigFactory {
 
     @Override
     public <T> T createConfig(String name, Class<T> configClass) {
-        return ConfigGenerator.newInstance(configClass, new ConfigurateConfig(new File(plugin.getDataFolder(), name + ".hocon"), HoconConfigurationLoader.builder()));
+        return ConfigGenerator.newInstance(configClass, new ConfigurateConfig(new File(plugin.getDataFolder(), name + ".conf"), HoconConfigurationLoader.builder()));
     }
 }
